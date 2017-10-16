9bc79af46a5fd0261f5c35a997b1a0942ab01483
Meuyeah <cursosrjorge@gmail.com>
2017-10-16 16:32:19 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,10 +1,14 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+
 install.packages("dplyr")
 library(dplyr)
+
 NEIbalt<- filter(NEI,fips=="24510")
 aggBal <- aggregate(Emissions ~ year+type, NEIbalt,sum)
+
 png("plot3.png",width= 480, height = 480)
+
 g <- qplot(year, Emissions, data = aggBal, facets = .~ type) + geom_smooth(method = "lm") + ggtitle("Baltimore emissions by source")
- print(g)
+print(g)
 dev.off()

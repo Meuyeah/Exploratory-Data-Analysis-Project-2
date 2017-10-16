e90f7cf62315f8d8e612947a5e075b76cd085c63
Meuyeah <cursosrjorge@gmail.com>
2017-10-16 16:33:30 +0200

    Update plot5.R

--- plot5.R
+++ plot5.R
@@ -1,9 +1,13 @@
 NEI <- readRDS("summarySCC_PM25.rds")
+
 install.packages("dplyr")
 library(dplyr)
+
 NEIbalt <- filter(NEI, fips == 24510 & type == "ON-ROAD")
 agg <- aggregate(Emissions ~ year, NEIbalt,sum)
+
 png("plot5.png",width=480,height=480)
+
 g <- qplot(year, Emissions, data = agg) + geom_smooth(method = "lm") + ggtitle("Baltimore emissions by vehicles")
  print(g)
  dev.off
